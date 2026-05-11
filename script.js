b1d029b2562713bb296e25ab704109ea22495032
SplashbroJerm <122891971+SplashbroJerm@users.noreply.github.com>
2026-05-11 14:28:15 -0700

    Change player coordinates and render image
    Updated player position and added image rendering.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const player = document.getElementById('player');
 
 const ctx = player.getContext("2d");
+const img = new Image();
+img.src = "squirtle-sprite2.png"
 
-let x = 0;
-let y = 0;
+
+let x = -90;
+let y = 460;
 let vx = 0;
 let vy = 0;
 function update(){
     ctx.clearRect(0, 0, player.clientWidth, player.height);
     x += vx;
     y += vy;
-    ctx.fillRect(x, y, 50, 50);
+    ctx.drawImage(img, x, y, 300, 150);
     requestAnimationFrame(update);
 }
 
@@ -25,7 +28,7 @@ addEventListener("keyup", function(e){
     if(e.code == 'KeyD' || e.code == "ArrowRight")vx = 0;
     if(e.code == 'KeyA' || e.code == "ArrowLeft")vx = 0;
     if(e.code == 'KeyS' || e.code == "ArrowDown")vy = 0;
-    if(e.code == 'KeyW' || e.code == "ArrowUp")vy = 0;
+    if(e.code == 'KeyW' || e.code == "ArrowUp")vy = 5;
 });
 
 
