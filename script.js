fc644b1240bb9aa04bf76333815dda7277d19584
SplashbroJerm <122891971+SplashbroJerm@users.noreply.github.com>
2026-05-13 21:02:46 -0700

    Improve player control and image handling
    Refactor player movement and image loading logic.

--- script.js
+++ script.js
@@ -1,55 +1,99 @@
 const player = document.getElementById('player');
-
 const ctx = player.getContext("2d");
-const img = new Image();
-img.src = "squirtle-sprite2.png"
 
+const img  = new Image();
+const img2 = new Image();
 
-let x = -90;
-let y = 650;
-let vx = 0;
-let vy = 0;
-let jumpForce = -10;
+let loaded = 0;
+function onLoad() { if (++loaded === 2) update(); }
+img.onload  = onLoad;
+img2.onload = onLoad;
+img.src  = "squirtle-sprite2.png";
+img2.src = "pika-plat.png";
+
+let x = 0, y = 500;
+let vx = 0, vy = 0;
 let grounded = false;
-const gravity = 0.5;
-const groundY = 650;
-const spriteH = 150;
 
-function update() {
-    if (!grounded) {
-      vy += gravity;
-    }
-    ctx.clearRect(0, 0, player.width, player.height);
-    x += vx;
-    y += vy;
-    ctx.drawImage(img, x, y, 300, spriteH);
-    if (y + spriteH > groundY) {
-      y = groundY - spriteH;
-      vy = 0;
-      grounded = true;
-    } else {
-      grounded = false;
-    }
-    requestAnimationFrame(update);
+const jumpForce = -14;
+const gravity   = 0.5;
+const groundY   = 650;
+const playerW   = 120;
+const spriteH   = 120;
 
+const platforms = [
+    {x: 400, y:560, w:200, h:15},
+    {x: 600, y:470, w:200, h:15},
+    {x: 200, y:370, w:200, h:50}
+];
+
+const keys = {right: false, left: false};
+
+function overlaps(rect1, rect2) {
+    return rect1.x < rect2.x + rect2.w && rect1.x + rect1.w > rect2.x && rect1.y < rect2.y + rect2.h && rect1.y + rect1.h>rect2.y;
 }
 
-addEventListener("keydown", function(e){
-    if(e.code == 'KeyD' || e.code == "ArrowRight") vx = 5;
-    if(e.code == 'KeyA' || e.code == "ArrowLeft") vx = -5;
-    if(e.code == 'KeyS' || e.code == "ArrowDown") vy = 5;
-    if((e.code == 'KeyW' || e.code == "ArrowUp") && grounded) {
+function update() {
+    vx = 0;
+    if(keys.right) vx=5;
+    if(keys.left) vx = -5;
+
+    x += vx;
+    if (x < 0) x = 0;
+    if (x + playerW > player.width) x = player.width - playerW;
+
+    let pBox = { x: x, y: y, w: playerW, h: spriteH };
+    for (const plat of platforms) {
+        if (overlaps(pBox, plat)) {
+        if (vx > 0) x = plat.x - playerW;
+        if(vx < -0) x = plat.x + plat.w;
+        pBox.x = x;
+    }
+    }
+
+    if (!grounded) vy += gravity;
+    y += vy;
+    grounded = false;
+
+    pBox.y = y;
+    for (const plat of platforms) {
+        if (overlaps(pBox, plat)) {
+            if (vy >0) { 
+                    y = plat.y - spriteH; 
+                    grounded = true;
+                    vy=0;
+                } else if (vy < 0) {
+                    y = plat.y + plat.h;
+                    vy = 0;
+                }
+                pBox.y = y
+    }
+    }
+
+    if (y + spriteH >= groundY) {
+        y = groundY - spriteH;
+        vy = 0;
+        grounded = true;
+    }
+
+    ctx.clearRect(0, 0, player.width, player.height);
+    for(const plat of platforms) {
+        ctx.drawImage(img2, plat.x, plat.y, plat.w, 50);
+    }
+    ctx.drawImage(img,  x, y, playerW, spriteH);
+    requestAnimationFrame(update);
+}
+
+addEventListener("keydown", e => {
+    if (e.code === 'KeyD' || e.code === 'ArrowRight') keys.right = true;
+    if (e.code === 'KeyA' || e.code === 'ArrowLeft')  keys.left = true;
+    if ((e.code === 'KeyW' || e.code === 'ArrowUp') && grounded) {
         vy = jumpForce;
         grounded = false;
     }
 });
 
-addEventListener("keyup", function(e){
-    if(e.code == 'KeyD' || e.code == "ArrowRight") vx = 0;
-    if(e.code == 'KeyA' || e.code == "ArrowLeft") vx = 0;
-    if(e.code == 'KeyS' || e.code == "ArrowDown") vy = 0;
+addEventListener("keyup", e => {
+    if (e.code === 'KeyD' || e.code === 'ArrowRight') keys.right=false;
+    if (e.code === 'KeyA' || e.code === 'ArrowLeft')  keys.left=false;
 });
-
-
-
-img.onload = update;
